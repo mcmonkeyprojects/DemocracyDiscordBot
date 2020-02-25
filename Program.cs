5e7d0960f5c23c82bd8d705afdd26fe9e9027a21
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-02-24 23:31:21 -0800

    initial commit: blank project based on Discord template

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Discord.Net;
+using Discord;
+using Discord.WebSocket;
+using System.Diagnostics;
+using FreneticUtilities.FreneticExtensions;
+using FreneticUtilities.FreneticDataSyntax;
+
+namespace DemocracyDiscordBot
+{
+    /// <summary>
+    /// General program entry and handler.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Software entry point - starts the bot.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // TODO
+        }
+    }
+}
