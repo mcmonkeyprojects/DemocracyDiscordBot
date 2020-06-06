41dc61a4e3ee5ad1ad7ef248b8dbb54de536827e
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-05 19:08:21 -0700

    name consistency fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull origin master
 git submodule update --init --recursive
-dotnet build DiscordDemocracyBot.sln --configuration Release -o ./bin/live_release
-screen -dmS DiscordDemocracyBot dotnet bin/live_release/DiscordDemocracyBot.dll -- $1
+dotnet build DemocracyDiscordBot.sln --configuration Release -o ./bin/live_release
+screen -dmS DemocracyDiscordBot dotnet bin/live_release/DemocracyDiscordBot.dll -- $1
