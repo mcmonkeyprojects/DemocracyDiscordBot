0da2af11ad93950057c0cc9fe83e1160705f0304
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-05 21:16:33 -0700

    fix start script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull origin master
 git submodule update --init --recursive
 dotnet build DemocracyDiscordBot.sln --configuration Release -o ./bin/live_release
-screen -dmS DemocracyDiscordBot dotnet bin/live_release/DemocracyDiscordBot.dll -- $1
+screen -dmS DemocracyDiscordBot dotnet bin/live_release/DemocracyDiscordBot.dll $1
